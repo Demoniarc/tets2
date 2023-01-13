f6933bf8b67c3f22c3db8d85e45095d5ac491919
Tom <107487368+Demoniarc@users.noreply.github.com>
2023-01-13 19:19:27 +0200

    Add files via upload

---
+++ ML22 - Copie.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import json
+import os
+import sys
+
+
+def get_input(local=False):
+    if local:
+        print("Reading local file")
+        
+        return "tkt.csv"
+
+    dids = os.getenv("DIDS", None)
+
+    if not dids:
+        print("No DIDs found in environment. Aborting.")
+        return
+
+    dids = json.loads(dids)
+
+    for did in dids:
+        filename = f"data/inputs/{did}/0"  # 0 for metadata service
+        print(f"Reading asset file {filename}.")
+
+        return filename
+
+def run_gpr(local=False):
+        
+    filename = get_input(local)
+    if not filename:
+        print("Could not retrieve filename.")
+        return
+    
+    print("ouiq")
+    
+    f = open("/data/outputs/result", "w")
+    f.write("fdhjgiuy")
+    f.close()   
+
+if __name__ == '__main__':
+    local = len(sys.argv) == 2 and sys.argv[1] == "local"
+    run_gpr(local)
